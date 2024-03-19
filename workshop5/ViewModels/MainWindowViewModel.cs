325b1a7533e683ef3b8026bdfa99c1b008d62450
BalintBihon <balint.bihon@gmail.com>
2024-03-19 09:05:58 +0100

    sokminden

---
+++ workshop5/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Backend.Model;
+using System.ComponentModel;
+using System.Windows;
+using System.Numerics;
+
+namespace workshop5.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        private string inputText = "";
+        private string sentby = "";
+        public string InputText
+        {
+            get { return inputText; }
+            set { inputText = value; OnPropertyChanged(); (SendCommand as RelayCommand).NotifyCanExecuteChanged(); }
+        }
+        public ICommand SendCommand { get; set; }
+        public RestCollection<Chat> Chats { get; set; }
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+        public MainWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Chats = new RestCollection<Chat>("http://localhost:53910/", "chat", "hub"); //majd névre figyelni
+                SendCommand = new RelayCommand(() =>
+                {
+                    Chats.Add(new Chat(sentby, InputText, DateTime.Now.ToString("MM/dd/yyyy h:mm tt")));
+                    inputText = "";
+                });
+            }
+        }
+    }
+}
