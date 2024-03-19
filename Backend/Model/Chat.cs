ecd20a397dad085eb1659ae2565224e2905a5dd9
bencsikbenceoenik <bencsikbence01@gmail.com>
2024-03-19 08:33:29 +0100

    ctor in model

--- Backend/Model/Chat.cs
+++ Backend/Model/Chat.cs
@@ -5,5 +5,13 @@
         public string Name { get; set; }
         public string message  { get; set; }
         public string Date { get; set; }
+
+        public Chat(string name, string message, string date)
+        {
+            Name = name;
+            this.message = message;
+            Date = date;
+        }
+
     }
 }
